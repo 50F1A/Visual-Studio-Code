ededcbb676563d9c8f3b5069b20b147c23f6cee4
Sofia <sofia.reis@estudante.iftm.edu.br>
2022-03-28 21:35:13 -0300

    CODIGO

---
+++ Exercício5.java
@@ -0,0 +1,11 @@
+public class Exercício5 {
+    public static void main (String[] args){
+        float dolar = 20;
+         
+        float reais = (float) (dolar * 5.08);
+
+        System.out.println(dolar + " dólares equivale a " + reais + " reais" );
+
+    }
+    
+}
