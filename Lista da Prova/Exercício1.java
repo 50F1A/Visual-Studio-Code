255468ba7002dccfa71f9e2d649b51f31f96c7ba
Sofia <sofia.reis@estudante.iftm.edu.br>
2022-04-11 22:28:57 -0300

    CODIGO

--- Lista da Prova/Exercício1.java
+++ Lista da Prova/Exercício1.java
@@ -3,23 +3,23 @@ import java.util.Scanner;
 public class Exercício1 {
     
     public static void main(String[] args) {
-        
-        Scanner entrada = new Scanner(System.in);
 
+        Scanner entrada = new Scanner(System.in);
+        
             System.out.print("Digite o nome do produto:");
-            String nome = entrada.nextLine();
+            String nome = new Scanner(System.in).nextLine();
 
             System.out.print("Digite o modelo do produto:");
-            String modelo = entrada.nextLine();
+            String modelo = new Scanner(System.in).nextLine();
 
             System.out.print("Digite o tamanho em cm do produto:");
-            float tamanho = entrada.nextFloat();
+            float tamanho = new Scanner(System.in).nextFloat();
 
             System.out.print("Digite o preço em reais do produto:");
-            double preço = entrada.nextDouble();
+            double preço = new Scanner(System.in).nextDouble();
 
             System.out.print("Digite a quantidade do produto:");
-            int quantidade= entrada.nextInt();
+            int quantidade= new Scanner(System.in).nextInt();
 
             System.out.println("Os dados dos produtos. " + 
             " Nome do produto: " + nome + ";" + 
