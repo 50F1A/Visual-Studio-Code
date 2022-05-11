96dedab0564ed173ecd046328e2f6c8d7eed8b12
Sofia <sofia.reis@estudante.iftm.edu.br>
2022-05-10 21:24:48 -0300

    CODIGO

--- Classe Math/AbsMath.java
+++ Classe Math/AbsMath.java
@@ -5,7 +5,7 @@ public class AbsMath {
     public static void main(String[] args) {
         Scanner entrada = new Scanner(System.in);
         
-        System.out.println("Valor absoluto do número");
+        System.out.println("ME INFORME UM NÚMERO QUE IREI MOSTRAR O VALOR ABSOLUTO DELE");
 
         System.out.print("Digite um número: ");
         int num = entrada.nextInt();
