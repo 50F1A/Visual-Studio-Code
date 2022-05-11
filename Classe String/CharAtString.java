4d35471fe298db8063a3ff6a7d50cba856dff0a2
Sofia <sofia.reis@estudante.iftm.edu.br>
2022-05-10 22:19:38 -0300

    CODIGO

---
+++ Classe String/CharAtString.java
@@ -0,0 +1,11 @@
+public class CharAtString {
+
+    public static void main(String[] args) {
+        
+
+        String valor = "LABORATÓRIO DE PROGRAMAÇÃO II";
+        System.out.println(valor.charAt(3));
+
+    }
+    
+}
