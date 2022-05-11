637682dbd23db5f21247b213c285799b2b4d6243
Sofia <sofia.reis@estudante.iftm.edu.br>
2022-05-10 21:28:56 -0300

    CODIGO

--- Classe Math/CeilMath.java
+++ Classe Math/CeilMath.java
@@ -5,7 +5,7 @@ public class CeilMath {
     public static void main(String[] args) {
         Scanner entrada = new Scanner(System.in);
         
-        System.out.println("Me dê um número que irei arredondar o valor para cima");
+        System.out.println("ME INFORME UM NÚMERO QUE IREI ARREDONDAR O VALOR PARA CIMA");
 
         System.out.print("Digite um número: ");
         double num = entrada.nextDouble();
