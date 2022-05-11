1835c0f6a4cb9f9f4bc7ab3b4c48f2d3895509e1
Sofia <sofia.reis@estudante.iftm.edu.br>
2022-05-10 22:25:25 -0300

    CODIGO

---
+++ Classe String/StartsWithString.java
@@ -0,0 +1,10 @@
+public class StartsWithString {
+
+    public static void main(String[] args) {
+        
+        String valor = "Salário do Funcionário";
+
+        System.out.println(valor.startsWith("Salário"));
+    }
+    
+}
